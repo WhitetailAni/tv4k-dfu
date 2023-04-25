15ff7cf11add2694b9f48b613ad7b6f0c148c48e
WhitetailAni <davidmichael2006@gmail.com>
2023-04-25 14:46:56 -0500

    simplify script

--- dfu.ino
+++ dfu.ino
@@ -1,5 +1,3 @@
-int setupMessage = 0;
-
 void setup() {
   Serial.begin(115200);
   pinMode(6, OUTPUT);
@@ -7,17 +5,16 @@ void setup() {
 }
 
 void loop() {
+  Serial.print("Waiting for start...");
   while (digitalRead(8) == LOW) {
-    //if(setupMessage = 0){
-      Serial.print("Waiting for start...");
-      setupMessage = 1;
-    //}
   }
+  
   Serial.println("Resetting device");
   analogWrite(6, 66); //reset
+  
   Serial.println("Waiting 0.5 seconds");
   delay(500);
+  
   Serial.println("Setting force_dfu");
   analogWrite(6, 40); //force_dfu
-  setupMessage = 0;
 }
